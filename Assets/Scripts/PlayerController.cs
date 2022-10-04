4a366c99909efbfee5541717c3f030fb07d87bb1
CASTEL-Florian <106156391+CASTEL-Florian@users.noreply.github.com>
2022-10-04 12:58:19 +0200

    Wall jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,16 +11,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float groundDeceleration;
     [SerializeField] private float airAcceleration;
     [SerializeField] private float airDeceleration;
+    [SerializeField] private float maxFallingSpeedOnWalls = 2f;
     [Header("Saut")]
     [SerializeField] private float gravity = 20;
     [SerializeField] private int airJumpCount = 1;
     [SerializeField] private float maxVelocityY;
     [SerializeField] private float jumpVelocity;
+    [SerializeField] private Vector2 wallJumpVelocity;
     [SerializeField] private bool airControl = true;
     [Header("Collisions verticales et horizontales")]
     [SerializeField] private BoxCollider2D groundCheck;
     [SerializeField] private float groundCheckRadius;
     [SerializeField] private LayerMask m_WhatIsGround;
+    [SerializeField] private LayerMask m_WhatIsWalls;
+    [SerializeField] private BoxCollider2D wallCheck;
+    [SerializeField] private Transform wallCheckTransform;
     [Header("Autre")]
 
     private int currentAirJumpCount;
@@ -30,6 +35,7 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveDirection;
     private BoxCollider2D boxCollider;
     private bool grounded = false;
+    private bool onWall = false;
     private Vector2 lastPosition;
     private void Start()
     {
@@ -44,13 +50,28 @@ public class PlayerController : MonoBehaviour
         if (jump)
         {
             jump = false;
-            velocity.y = jumpVelocity;
+            if (onWall)
+            {
+                velocity = wallJumpVelocity;
+                if (wallCheck.transform.rotation.eulerAngles.z < 90)
+                    velocity.x = -velocity.x;
+            }
+            else
+            {
+                velocity.y = jumpVelocity;
+            }
         }
-        velocity.y -= gravity * Time.fixedDeltaTime;
         if (airControl || grounded)
             velocity.x += moveDirection.x * Time.fixedDeltaTime * (grounded ? groundAcceleration : airAcceleration);
         velocity.x = Mathf.Clamp(velocity.x, -maxVelocityX, maxVelocityX);
+        wallCheckTransform.rotation = Quaternion.Euler(0,0,velocity.x < 0 ? 180 : 0);
+
+        WallCheck();
+        velocity.y -= gravity * Time.fixedDeltaTime;
         velocity.y = Mathf.Clamp(velocity.y, -maxVelocityY, maxVelocityY);
+        if(onWall)
+            velocity.y = Mathf.Clamp(velocity.y, -maxFallingSpeedOnWalls, maxVelocityY);
+
         float deceleration = grounded ? groundDeceleration : airDeceleration;
         if (moveDirection.x == 0)
         {
@@ -78,9 +99,24 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void WallCheck()
+    {
+        onWall = false;
+        if (velocity.x * moveDirection.x <= 0)
+            return;
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(wallCheck.transform.position, wallCheck.size, 0, m_WhatIsWalls);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                onWall = true;
+            }
+        }
+    }
+
     private void Jump()
     {
-        if (grounded)
+        if (grounded || onWall)
         {
             jump = true;
         }    
