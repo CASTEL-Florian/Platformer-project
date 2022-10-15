f21313fc2810f3d1ddc1df6b34e8a91e394680ca
CASTEL-Florian <106156391+CASTEL-Florian@users.noreply.github.com>
2022-10-15 20:35:31 +0200

    Controls on main menu

--- Assets/Scripts/SelectionArrow.cs
+++ Assets/Scripts/SelectionArrow.cs
@@ -5,8 +5,14 @@ using UnityEngine.UI;
 
 public class SelectionArrow : MonoBehaviour
 {
-    [SerializeField] private List<Button> buttons;
-    [SerializeField] private List<float> offsets;
+    [System.Serializable]
+    public class Tab
+    {
+        public List<Button> buttons;
+        public List<float> offsets;
+    }
+    [SerializeField] private List<Tab> tabs;
+    private int tabIndex = 0;
     private int buttonIndex = 0;
     private void Start()
     {
@@ -16,30 +22,37 @@ public class SelectionArrow : MonoBehaviour
     {
         buttonIndex -= 1;
         if (buttonIndex < 0)
-            buttonIndex = buttons.Count - 1;
+            buttonIndex = tabs[tabIndex].buttons.Count - 1;
         UpdatePos();
     }
     public void GoDown()
     {
         buttonIndex += 1;
-        if (buttonIndex >= buttons.Count)
+        if (buttonIndex >= tabs[tabIndex].buttons.Count)
             buttonIndex = 0;
         UpdatePos();
     }
 
     public void GoToButton(Button button)
     {
-        buttonIndex = buttons.IndexOf(button);
+        buttonIndex = tabs[tabIndex].buttons.IndexOf(button);
         UpdatePos();
     }
 
     private void UpdatePos()
     {
-        transform.position = new Vector2(buttons[buttonIndex].transform.position. x + offsets[buttonIndex], buttons[buttonIndex].transform.position.y);
+        transform.position = new Vector2(tabs[tabIndex].buttons[buttonIndex].transform.position. x + tabs[tabIndex].offsets[buttonIndex], tabs[tabIndex].buttons[buttonIndex].transform.position.y);
     }
 
     public void PressButton()
     {
-        buttons[0].onClick.Invoke();
+        tabs[tabIndex].buttons[buttonIndex].onClick.Invoke();
+    }
+
+    public void SwitchTab(int newTabIndex)
+    {
+        tabIndex = newTabIndex;
+        buttonIndex = 0;
+        UpdatePos();
     }
 }
